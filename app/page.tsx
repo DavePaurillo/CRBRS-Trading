5a526bdeeef3e390fb9c94b26374611804a501e6
DavePaurillo <paurillo.dave@gmail.com>
2024-06-08 20:36:55 +0800

    Testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import { Hero } from "./Hero";
 import { About } from "./About";
 import { POC } from "./POC";
 import { HeroBackground } from "./components/ui/hero-highlight";
+import { Testimonials } from "./Testimonials";
 
 export default function Home() {
 	return (
 		<main>
-			{/* Nav and Hero section */}
 			<HeroBackground>
 				<Nav />
 				<Hero />
@@ -15,7 +15,9 @@ export default function Home() {
 				<About />
 				<div className='h-64'></div>
 				<POC />
-				<div className='h-64'></div>
+				<div className='h-48'></div>
+				<Testimonials />
+				<div className='h-48'></div>
 			</HeroBackground>
 		</main>
 	);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
 			},
 			animation: {
 				shimmer: "shimmer 2s linear infinite",
+				scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
 			},
 			keyframes: {
 				shimmer: {
@@ -32,6 +33,11 @@ const config: Config = {
 					},
 					to: {
 						backgroundPosition: "-200% 0",
+					},
+				},
+				scroll: {
+					to: {
+						transform: "translate(calc(-50% - 0.5rem))",
 					},
 				},
 			},
