51781a007173d10d11f63cc83713c82f04b9e9a5
DavePaurillo <paurillo.dave@gmail.com>
2024-08-04 19:40:16 +0800

    Bootcamp

--- app/bootcamp/BootcampRecordings.tsx
+++ app/bootcamp/BootcampRecordings.tsx
@@ -1,7 +1,115 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import backarrow from "../../public/back-arrow-icon.svg";
 
 const BootcampRecordings = () => {
-	return <div>BootcampRecordings</div>;
+	const [selectedRecordingLink, setSelectedRecordingLink] = useState(
+		"https://drive.google.com/file/d/1T98aO4hebPOnYxXgOwcjZ-kQPuzNsWwy/preview"
+	);
+	const [selectedRecordingTitle, setSelectedRecordingTitle] = useState(
+		"Esther - Onboarding"
+	);
+
+	const handleClick = (link: string, title: string) => {
+		setSelectedRecordingLink(link);
+		setSelectedRecordingTitle(title);
+		window.scrollTo(0, 0);
+	};
+
+	const videolist = [
+		{
+			link: "https://drive.google.com/file/d/1T98aO4hebPOnYxXgOwcjZ-kQPuzNsWwy/preview",
+			title: "Esther - Onboarding",
+		},
+		{
+			link: "https://drive.google.com/file/d/1UYP4nMzjlMhT__LSbiHpZyMfMslE_dcE/preview",
+			title: "Esther - Day 1",
+		},
+		{
+			link: "https://drive.google.com/file/d/1xjfAXVCGYWOa_rCzj-WivUDeGptLLNcB/preview",
+			title: "Esther - Day 2",
+		},
+		{
+			link: "",
+			title: "Esther - Day 3",
+		},
+		{
+			link: "https://drive.google.com/file/d/1sx5uWupSJF4IA4SfhvPkUHIOr0h25gED/preview",
+			title: "Esther - Day 4",
+		},
+		{
+			link: "https://drive.google.com/file/d/1Ye-Gf_e6r2vxWPc6tWIXtUK48aKgzOfj/preview",
+			title: "Esther - Day 5",
+		},
+		{
+			link: "https://drive.google.com/file/d/1fV6X68MNMkTDMaZgbn3Qzt6XrDaxmevj/preview",
+			title: "Esther - Day 6",
+		},
+	];
+
+	return (
+		<div className='h-full w-screen'>
+			<div className='max-w-7xl mx-auto pt-12'>
+				<Link href='/' className='flex items-center -ml-24 mb-12'>
+					<Image
+						src={backarrow}
+						alt='back-arrow'
+						className='h-4 w-auto mr-6'
+					/>
+					<p className='text-neutral-200'>Go back to home page</p>
+				</Link>
+				<h1 className='text-2xl md:text-3xl pl-2 my-2 border-l-4 font-bold border-crbrsOrange text-gray-200 mb-4 max-w-7xl mx-auto rounded-sm tracking-widest'>
+					&nbsp; Bootcamp - Esther
+				</h1>
+
+				{/* main video */}
+				<section className='mb-8'>
+					<iframe
+						src={selectedRecordingLink}
+						width='auto'
+						height='auto'
+						allow='autoplay'
+						allowFullScreen
+						loading='lazy'
+						sandbox='allow-same-origin allow-scripts'
+						className='w-full h-[800px] p-4 border-4 border-crbrsDark rounded-lg'
+					/>
+					<h1 className='text-lg my-2 font-bold text-gray-200 mb-4 max-w-7xl mx-auto rounded-sm tracking-widest'>
+						{selectedRecordingTitle}
+					</h1>
+				</section>
+
+				<section className='flex flex-wrap justify-center gap-10 pb-24'>
+					{videolist.map((video, i) => {
+						return (
+							<div
+								onClick={() =>
+									handleClick(video.link, video.title)
+								}
+								key={i}
+							>
+								<iframe
+									src={video.link}
+									width='auto'
+									height='auto'
+									allow='autoplay'
+									allowFullScreen
+									loading='lazy'
+									sandbox='allow-same-origin allow-scripts'
+									className='p-4 border-4 border-crbrsDark rounded-lg pointer-events-none cursor-pointer'
+								/>
+								<h1 className='text-sm my-2 font-bold text-gray-200 mb-4 max-w-7xl mx-auto rounded-sm tracking-widest'>
+									{video.title}
+								</h1>
+							</div>
+						);
+					})}
+				</section>
+			</div>
+		</div>
+	);
 };
 
 export default BootcampRecordings;
--- app/bootcamp/page.tsx
+++ app/bootcamp/page.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import BootcampRecordings from "./BootcampRecordings";
+import { HeroBackground } from "../components/ui/hero-highlight";
+import { auth } from "../firebase/firebaseConfig";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { db } from "../firebase/firebaseConfig";
+import { query, where, collection, getDocs } from "firebase/firestore";
 
 const Bootcamp = () => {
+	const [currentUser, setCurrentUser] = useAuthState(auth);
+	const [isValidUser, setIsValidUser] = useState(false);
+
+	const validateUser = async () => {
+		try {
+			const q = query(
+				collection(db, "users"),
+				where("emailAddress", "==", currentUser?.email)
+			);
+			const querySnapshot = await getDocs(q);
+			if (!querySnapshot.empty) {
+				querySnapshot.forEach((user) => {
+					if (currentUser?.email === user.data().emailAddress) {
+						if (user.data().isBootcampAttendee) {
+							setIsValidUser(true);
+						}
+					}
+				});
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	useEffect(() => {
+		validateUser();
+	});
+
 	return (
-		<div>
-			<BootcampRecordings />
-		</div>
+		<HeroBackground>
+			{isValidUser ? <BootcampRecordings /> : <div></div>}
+		</HeroBackground>
 	);
 };
 
